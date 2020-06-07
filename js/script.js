08b763bb7d33b25160fe21695d4bd4c2e620ad7c
Patryk <63202238+lol3k2008@users.noreply.github.com>
2020-06-07 12:55:43 +0200

    Animowane napisy

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
-const h1 = document.querySelector(`.h1`);
-const par = document.querySelectorAll('p')
-document.addEventListener('DOMContentLoaded', function(){
-    //czekamy az przegladarka wczyta caly document
-    h1.classList.add('active');
+//    animowane napisy
+//  By   ==     https://scrollrevealjs.org/guide/customization.html
+ScrollReveal().reveal('.headline');
+ScrollReveal({ reset: true });
 
-    function addShadow(){  //fucnkcja o nazwie addShadow
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.ha2', { delay: 2000 });
+ScrollReveal().reveal('.galery', { delay: 150 });
 
 
-       if(window.scrollY >= 1 ){  
-           h1.classList.add(`active`);
-       }else{
-           h1.classList.remove(`active`);
-       
-        }
-   }
-   
-   window.addEventListener('scroll', addShadow)
-   //Nasluchiwanie documentu na scroll, podczas scroll'a uruchomienie funckji addShadow
-   });
-   
+ScrollReveal().reveal('.photo', { delay: 800 });
+ScrollReveal().reveal('.photo1', { delay: 300 });
+ScrollReveal().reveal('.photo2', { delay: 600 });
+ScrollReveal().reveal('.photo3', { delay: 900 });
+ScrollReveal().reveal('.photo4', { delay: 1200 });
+
+
+
+ScrollReveal().reveal('.pE1', { delay: 1000 });
+ScrollReveal().reveal('.pE2', { delay: 1200 });
+ScrollReveal().reveal('.pE3', { delay: 1400 });
+
+
+
